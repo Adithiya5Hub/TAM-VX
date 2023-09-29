ed216e9a7aaa36060ee62fdd6e9734241569c155
S. Adithiya <adithiya1805@gmail.com>
2023-09-29 22:10:22 +0530

    NewCommit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,7 +10,7 @@ const Tech = () => {
     <>
       <motion.div variants={fadeIn("right", "spring")}>
         <img src="https://ik.imagekit.io/o3b14qow3gqi/Crew/princi_O8p_H9fUo_tS.jpg?updatedAt=1647430271746" alt="Principal" width="600px" className='rounded-lg'/>
-        <h3 className='sm:text-[18px] text-[14px] text-secondary tracking-wider font-bold'>| Board Of Director</h3>
+        <h3 className='sm:text-[18px] text-[14px] text-secondary tracking-wider font-bold'>| Board Of <span className='text-[#915eff]' >Director</span></h3>
         <h2 className='text-white font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>Dr.P.Santosh Kumar Patra</h2>
         <p className= "sm:text-[18px] text-[14px] text-secondary tracking-wider">Dr.P.Santosh Kumar Patra , Principal of St.Martin's Engineering College is holding B.E. in Computer Science & Engineering, MTech in Computer Science & Engineering and Ph.D in Computer Science & Engineering degrees. His area of interests are Airtificial Intelligence,Software Engineering , Data mining and warehousing & Wireless Networks.</p>
       </motion.div>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,7 +7,7 @@ const Works = () => {
     <motion.div variants={fadeIn("right", "spring")} >
       <img src="https://ik.imagekit.io/o3b14qow3gqi/Crew/sss_z642MOd4G.jpg?updatedAt=1647509408608" width="600px" className='rounded-lg'/>
       <br />
-      <h3 className='sm:text-[18px] text-[14px] text-secondary tracking-wider font-bold'>| Faculty Advisor</h3>
+      <h3 className='sm:text-[18px] text-[14px] text-secondary tracking-wider font-bold'>| Faculty <span className='text-[#915eff]'>Advisor</span></h3>
       <h2 className='text-white font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>E.Sowmya</h2>
       <p className= "sm:text-[18px] text-[14px] text-secondary tracking-wider">Every great organization has a great team and every great teams need a great Advisor. We as a crew at TAM have made it a point to ensure that every event is filled with a great deal of success, in addition to ensuring every participant is able to be served with equal priority. From planning the events to marketing, to making our website, we make sure that we are meeting the needs of every participant.</p>
     </motion.div>
