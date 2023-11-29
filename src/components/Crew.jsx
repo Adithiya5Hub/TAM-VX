cafe9d972647fdcc98d1cf34a699d44b6e9db33e
Kalyancric18 <kalyan.programming@gmail.com>
2023-11-29 12:46:02 +0530

    CREW PAGE

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,10 +1,115 @@
+import './statichtml/crew.css';
+
 const Crew = () => {
-    return(
+
+
+    return (
         <div>
-            <h1>Crew details</h1>
+            <div className="loading">
+                <main>
+                    <div className="frame"></div>
+                    <div className="grid" data-effect="hover-1">
+                        <h2 className="grid__title">
+                            Executives <br />
+                        </h2>
+                        <div className="card">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/3.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--c">
+                                <span className="card__box-number">02</span>
+                                <span className="card__box-tags">#techno #rave</span>
+                            </div>
+                            <div className="card__box card__box--d">
+                                <span className="card__box-category">Pulse Club</span>
+                            </div>
+                        </div>
+                        <div className="card card--empty" />
+                        <div className="card card--alt">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/22.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--a">
+                                <span className="card__box-number">07</span>
+                                <span className="card__box-tags">#house #deephouse</span>
+                            </div>
+                            <div className="card__box card__box--d">
+                                <span className="card__box-category">Stellar Lounge</span>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/2.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--b">
+                                <span className="card__box-number">09</span>
+                                <span className="card__box-tags">#dubstep #bass</span>
+                            </div>
+                            <div className="card__box card__box--c">
+                                <span className="card__box-category">Bass Arena</span>
+                            </div>
+                        </div>
+                        <div className="card card--empty" />
+                        <div className="card card--alt">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/21.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--b">
+                                <span className="card__box-number">13</span>
+                                <span className="card__box-tags">#trance #progressive</span>
+                            </div>
+                            <div className="card__box card__box--c">
+                                <span className="card__box-category">Euphoria Hall</span>
+                            </div>
+                        </div>
+                        <div className="card card--alt">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/17.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--b">
+                                <span className="card__box-number">17</span>
+                                <span className="card__box-tags">#drumandbass #jungle</span>
+                            </div>
+                            <div className="card__box card__box--d">
+                                <span className="card__box-category">Groove Hive</span>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div
+                                className="card__img"
+                                style={{
+                                    backgroundImage: "url(src/components/statichtml/img/20.jpg)"
+                                }}
+                            />
+                            <div className="card__box card__box--c">
+                                <span className="card__box-number">19</span>
+                                <span className="card__box-tags">#ambient #chillout</span>
+                            </div>
+                            <div className="card__box card__box--d">
+                                <span className="card__box-category">Tranquil Gardens</span>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+
         </div>
-    )
-}
+    );
+};
 
-
-export default Crew
+export default Crew;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { menu, close } from "../assets";
+
+
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+
   return (
-    <nav className={`${
-      styles.paddingX
-    } w-full flex items-center py-5 fixed top-0 z-20 ${
-       "bg-primary"
-    }`}>
+
+    <nav className={`${styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${"bg-primary"
+      }`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/'
           className='flex items-center gap-2'
@@ -19,60 +21,57 @@ const Navbar = () => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-            <p className='text-white text-[28px] font-bold cursor-pointer flex'>TAM &nbsp;<span className='sm:block hidden text-[#915eff]'>VX</span></p>
-          </Link>
-          <ul className='list-none hidden sm:flex flex-row gap-10'>
-            {navLinks.map((nav)=>(
-              <li
-                key={nav.id}
-                className={`${
-                  active === nav.title
-                    ? "text-white"
-                    : "text-secondary"
-               } hover:text-white text-[18px] font-medium`}
-               onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-            <Link to={'/experience'} target='_blank'> <li
-                className={`text-secondary hover:text-white text-[18px] font-medium`}>
-                <a>{'Register'}</a>
-              </li></Link>
-              <Link to={'/crew'} target='_blank'> <li
-                className={`text-secondary hover:text-white text-[18px] font-medium`}>
-                <a>{'Crew'}</a>
-              </li></Link>
-          </ul>
+          <p className='text-white text-[28px] font-bold cursor-pointer flex'>TAM &nbsp;<span className='sm:block hidden text-[#915eff]'>VX</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${active === nav.title
+                ? "text-white"
+                : "text-secondary"
+                } hover:text-white text-[18px] font-medium`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+          <Link to={'/experience'} target='_blank'> <li
+            className={`text-secondary hover:text-white text-[18px] font-medium`}>
+            <a>{'Register'}</a>
+          </li></Link>
+          <Link to={'/crew'} target='_blank'> <li
+            className={`text-secondary hover:text-white text-[18px] font-medium`}>
+            <a>{'Crew'}</a>
+          </li></Link>
+        </ul>
 
-          <div className='sm:hidden flex flex-1 justify-end items-center'>
-               <img src={toggle?close:menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
-               onClick={()=> setToggle(!toggle)} 
-               />
-            
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+
           <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            className={`${!toggle ? "hidden" : "flex"
+              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins  font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+                  className={`font-poppins  font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-secondary"
+                    }`}
                   onClick={() => {
                     setToggle(!toggle);
                     setActive(nav.title);
                   }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
-                  
+
                 </li>
               ))}
             </ul>
-          </div>      
+          </div>
         </div>
       </div>
     </nav>
